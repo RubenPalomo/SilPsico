2aa257e7ef6d89912bfaaeff8867306b3891861e
Rubén Palomo Fontán <Ruben.Palomof@gmail.com>
2024-04-06 16:23:36 +0200

    Reduction of the left margin of the Header

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import HeaderMenuComponent from "./HeaderMenuComponent/HeaderMenuComponent";
 
 export default function Header() {
   return (
-    <header className="flex justify-between w-full bg-white min-h-32 sm:min-h-0 pl-24 px-4 sm:px-24 py-8 sticky top-0 z-50">
+    <header className="flex justify-between w-full bg-white min-h-32 sm:min-h-0 pl-8 py-8 sticky top-0 z-50">
       <div>
         <h1 className="md:text-3xl font-bold">
           Silvia de Isidro Delgado
